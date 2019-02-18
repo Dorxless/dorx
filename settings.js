26793dc03c991eade233819f11ce01ac604c6e17
Dorx <36697194+Dorxless@users.noreply.github.com>
2019-02-18 19:44:50 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,20 +1,12 @@
 //Settings:
 
 const settings = {
-    token: '742088547:AAH390qBIT10tEhqHt7_VLrU08UO65DSFgY',
+    token: '607964090:AAGQM_OgctlL04LiW6r7XtNhln1WINhdgVI',
     opt: {
         polling: true,
     },
     myId: '624107725',
-    wellcomMsg: `Welcome !!
-
-Any message you send will be delivered to the bot owner
-.
-.
-Don't worry , your identity will be coverd so you can talk freely 
-.
-.
-Follow us on @CottonMinds`
+    wellcomMsg: `talk to me :)`
 }
 
 module.exports = settings;
